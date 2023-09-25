1aa13859cb9854e371eae54580eb862d81654ed7
tsy <tsy@mail.com>
2023-09-25 14:47:42 +0300

    create :PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
+import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
-  plugins: [react()],
+  plugins: [VitePWA({ registerType: 'autoUpdate' })],
 })
